a9ffc2270440be68ddeb1b01aff0d30e1adb6f4e
Steven Ha <stevenha@berkeley.edu>
2018-07-25 18:14:20 -0700

    added photo slides

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
+	path('services/', views.services, name='services'),
+	path('gallery/', views.gallery, name='gallery'),
 ]
+
+handler404 = views.handler404
+handler505 = views.handler505
+handler500 = views.handler500
--- home/views.py
+++ home/views.py
@@ -5,8 +5,21 @@ app_name = "home"
 # Create your views here.
 def index(request):
 	# return HttpResponse("You're at the Blossom Nails Spa home page")
-	return render(request, 'index.html')
+	# string_left = "<div class=\"caro-image\"></div>{\% load staticfiles" + \
+	# 	" \%}<img src=\"{\% static '"
+	# string_right = ".jpg' \%}\"></div>"
+	names = []
+	for i in range(1, 18):
+		string = 'home/images/' + str(i) + '.jpg'
+		names.append(string)
+	return render(request, 'index.html', {'src_names': names})
 
+def services(request):
+	return render(request, 'services.html')
+
+def gallery(request):
+	return render(request, 'gallery.html')
+	
 def handler404(request):
 	return render(request, 'index.html')
 
