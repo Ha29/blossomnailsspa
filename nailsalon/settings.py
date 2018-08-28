3e0009d568c72303146d1e711ef39f430bc28420
Steven Ha <stevenha@berkeley.edu>
2018-08-28 15:58:49 -0700

    changed secret key

--- nailsalon/settings.py
+++ nailsalon/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tbgev)&hud&(b!4k$$f_ed27efgmy(2-b07&7v&p(r(=wxd9^6'
-# with open('secret_key.txt') as f:
-# 	SECRET_KEY = f.read().strip()
+# SECRET_KEY = 'tbgev)&hud&(b!4k$$f_ed27efgmy(2-b07&7v&p(r(=wxd9^6'
+with open('secret_key.txt') as f:
+	SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['tiem-nail.herokuapp.com', '127.0.0.1']
 
