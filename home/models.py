9a3fdad8e1771595d605d3fbaa86a0a01e84b833
Steven Ha <stevenha@berkeley.edu>
2019-02-22 00:37:13 +0000

    forms

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+	first_name = models.CharField(max_length=20)
+	last_name = models.CharField(max_length=20)
+	email = models.EmailField(primary_key=True, max_length=254)
+	is_admin = models.BooleanField(default=False)
